8aae3b055b9057ee0a9366cac65cb1c3682db5da
Jason Rauchwerk <jrauchwerk@gmail.com>
2023-11-06 20:43:42 -0500

    Added base class for ICL Retrievers

---
+++ retrievers/ICLRetrieverBase.py
@@ -0,0 +1,19 @@
+class ICLRetrieverBase:
+    # random monolingual
+    # random multilingual
+    # bm25 monolingual
+    # bm25 translated
+    # topk monolingual embeddings
+    # topk multilingual embeddings
+    # topk translated embeddings
+
+    # translate with nllb, mt5
+    # multilingual embeddings: sentence-transformers/stsb-xlm-r-multilingual
+    def __init__(self, data):
+        #{"text":str ,"label":int ,"model": str,"source": str,"id": int}
+        # possible values of source: {'arxiv', 'wikihow', 'reddit', 'chinese', 'peerread', 'indonesian', 'bulgarian', 'wikipedia', 'urdu'}
+        raise NotImplementedError
+    
+    def __call__(self, input_sentence: str, k: int) -> list[tuple[str, int]]:
+        # returns [(text1, label1), (text2, label2), ..., (textk, labelk)]
+        raise NotImplementedError
