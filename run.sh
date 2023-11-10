00c0573c3e3f08bae6a9d19702b5f804755b06b9
Sherlock-Jerry <rraghav5600@gmail.com>
2023-11-10 05:57:08 +0530

    Updates

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
+retriever="Random"
+
+mkdir -p data/SubtaskA/outputs/$retriever
 python -u main.py \
-    --retriever_name Random \
-    --test_file data/SubtaskA/subtaskA_dev_multilingual_processed_combined.jsonl \
-    --output_file data/SubtaskA/outputs/subtaskA_dev_multilingual_processed_combined.jsonl \
+    --retriever_name $retriever \
+    --test_file "data/SubtaskA/subtaskA_dev_multilingual_processed_combined.jsonl" \
+    --output_file "data/SubtaskA/outputs/$retriever/predictions.jsonl" \
     --k 1 \
     --in_language False
