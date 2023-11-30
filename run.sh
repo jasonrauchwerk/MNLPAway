cf534349ddb713adccc2a391574aee07940e5438
Sherlock-Jerry <rraghav5600@gmail.com>
2023-11-30 07:24:42 +0530

    Random Retriever Completed

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
-retriever="Random"
+# BM25Monolingual BM25Translated Embeddings TranslationEmbeddings
 
-mkdir -p data/SubtaskA/outputs/$retriever
-python -u main.py \
-    $retriever \
-    "data/SubtaskA/subtaskA_dev_multilingual_processed_combined.jsonl" \
-    "data/SubtaskA/outputs/$retriever/predictions.jsonl" \
-    1 \
-    False
+for retriever in "Embeddings"
+do
+    echo $retriever
+    mkdir -p data/SubtaskA/outputs/$retriever
+    python -u main.py \
+        $retriever \
+        "data/SubtaskA/subtaskA_dev_multilingual_processed_combined.jsonl" \
+        "data/SubtaskA/outputs/$retriever/predictions.jsonl" \
+        1 \
+        False
+done
