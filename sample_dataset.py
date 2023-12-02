65b74e354c8126e4d1c950a3dd743190c5afffcd
Jason Rauchwerk <jrauchwerk@gmail.com>
2023-12-02 17:25:13 -0500

    Dataset downsampling script
    Co-authored-by: parthsr5 <parthsr5@users.noreply.github.com>

---
+++ sample_dataset.py
@@ -0,0 +1,18 @@
+import sys
+from collections import defaultdict
+import json
+import random
+
+data = defaultdict(list)
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        j = json.loads(line)
+        data[(j['source'], j['label'])].append(line)
+
+for _, li in data:
+    random.shuffle(li)
+
+with open(sys.argv[2], 'w') as f:
+    for _, li in data:
+        for line in li[::4]:
+            f.write(line)
